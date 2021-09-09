da7563d56692348baac7d57a4468e5ba3c1db177
Danila <karantinz@mail.ru>
2021-09-09 14:49:11 +0300

    Добавьте файлы проекта.

---
+++ Task6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiacSign;
+            string work;
+
+            Console.Write("Как вас зовут - ");
+            name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Сколько вам лет - ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какой ваш знак зодиака - ");
+            zodiacSign = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Где вы работаете - ");
+            work = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Вас зовут " + name + ", вам " + age + " год," + " вы " + zodiacSign + " и работаете на " + work);
+            Console.ReadKey();
+        }
+    }
+}
